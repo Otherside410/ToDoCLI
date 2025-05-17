0db14f1ef898d3aa41c122cf3c893ea5bbe3e43f
Otherside <github.splashy124@passmail.net>
2025-05-17 22:58:39 +0200

    added code and match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    // affichage du menu
+    let actions = ["Créer une nouvelle liste", "Mettre à jour une liste existante", "Supprimer une liste existante", "Quitter"];
+    display_actions(&actions);
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)     // lit la ligne et la met dans `input`
+        .expect("Erreur de lecture");                  // Affiche un message si ça plante
+
+    // convertis l'entrée en nombres
+    let choix: u32 = input.trim().parse().expect("Veuillez entrer un nombre");
+
+    // utilisation de match pour exécuter une action selon le choix
+    match choix {
+        1 => {
+            println!("Création de liste...");
+            // ici tu pourrais appeler une fonction `creer_liste();`
+        },
+        2 => println!("Quelle liste souhaitez vous mettre à jour ?"),
+        3 => println!("Quelle liste souhaitez vous supprimer ?"),
+        _ => println!("Choix invalide."),
+    }
+}
+
+// fonction pour afficher le menu
+fn display_actions(actions: &[&str]) {
+    println!("Bonjour! Choisissez une action parmis ce que vous souhaitez faire :");
+    for (i, actions) in actions.iter().enumerate() {
+        println!("{} - {}", i + 1, actions);
+    }
 }
